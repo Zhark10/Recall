c76c5f96cf49b442066975542e476b38761788ad
Zhark10 <53864466+Zhark10@users.noreply.github.com>
2019-09-28 22:55:16 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/views/HomeScreen/HomeScreen";
 import HistoryScreen from "./src/views/HistoryScreen/HistoryScreen";
 
+(console as any).disableYellowBox = true;
+
+const stackNavigationConfig: { [key: string]: any } = {
+  headerMode: "none"
+};
+
 const MainNavigator = createStackNavigator({
-    Home: {screen: HomeScreen},
-    Profile: {screen: HistoryScreen},
-});
+  Home: { screen: HomeScreen },
+  History: { screen: HistoryScreen }
+}, stackNavigationConfig);
 
 const App = createAppContainer(MainNavigator);
 
